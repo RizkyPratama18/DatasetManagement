bc2c6975078db55bd3dbbf428f74fd1ae1c1c332
kiki <kiki.sby@gmail.com>
2021-01-24 20:48:19 +0700

    Register Auto Direct

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Auth;
+
 
 class RegisterController extends Controller
 {
@@ -28,7 +30,16 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+
+
+        protected function redirectTo()
+        {
+            if (Auth::User()->roles == 'Admin') {
+                return '/task/list';
+            }
+            return '/task/user';
+        }
+       protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
